ae6148d810587a13fb7305b37329ddcedc3a57f6
Нурсултан Ташанов <n.tashanov@sbereducation.ru>
2023-03-23 17:36:20 +0300

    ...

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -22,9 +22,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions):webpack.WebpackPlug
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
@@ -34,6 +31,9 @@ export function buildPlugins({ paths, isDev }: BuildOptions):webpack.WebpackPlug
             // overlay:false убирает iframe
             new ReactRefreshWebpackPlugin({ overlay: false }),
             // Плагин для анализа бандла
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
 
         );
     }
